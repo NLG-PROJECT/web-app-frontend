c2622aaca9fba8776e90546ae2188de644b537e3
poamen <pauloams16@gmail.com>
2025-03-17 08:03:51 +0000

    fixed grid lines

--- src/components/ParticleAnimation.tsx
+++ src/components/ParticleAnimation.tsx
@@ -265,9 +265,9 @@ export default function ParticleAnimation() {
         theme === 'dark' ? 'bg-black' : 'bg-white'
       }`}
     >
-      {/* Grid Background */}
-      <div className="absolute inset-0 h-full w-full z-0">
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#8884_1px,transparent_1px),linear-gradient(to_bottom,#8884_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#fff4_1px,transparent_1px),linear-gradient(to_bottom,#fff4_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+      {/* Grid Background - moved to root container */}
+      <div className="fixed inset-0 h-full w-full">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#8884_1px,transparent_1px),linear-gradient(to_bottom,#8884_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#fff4_1px,transparent_1px),linear-gradient(to_bottom,#fff4_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_50%,#000_40%,transparent_100%)]" />
       </div>
 
       <canvas
